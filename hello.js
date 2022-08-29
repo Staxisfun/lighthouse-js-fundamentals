a26be20d3c5cd5cd8f460a6b83feb5b20fe29193
Matt Wilson <mwilson82@hotmail.com>
2022-08-29 01:50:53 +0000

    intro to Functions

---
+++ hello.js
@@ -0,0 +1,11 @@
+/*const sayHello  = function () {
+  console.log("Hello, internet");
+}
+
+sayHello();*/
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Matt")
